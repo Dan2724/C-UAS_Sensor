ccd10759bf2a9d3ae5421543bc3883ee0b1cee2a
celardof <celardof@gmail.com>
2025-09-22 21:47:08 -0400

    UAS search update

--- UAS.m
+++ UAS.m
@@ -5,6 +5,7 @@ classdef UAS < handle
         target
         mode
         position
+        targetUnitVector
     end
     methods
         function obj = UAS(speed, entrance, target, mode)
@@ -14,18 +15,14 @@ classdef UAS < handle
             obj.mode = mode;
         end
 
-        function obj = linearMotion(obj,xPos0,yPos0,xPos,yPos,xtarget,ytarget,time,targetUnitVector)
-            obj.position.xPos = xPos0 + obj.speed*time*targetUnitVector(1);
-            obj.position.yPos = yPos0 + obj.speed*time*targetUnitVector(2);
+        function obj = linearMotion(obj,xPos0,yPos0,xPos,yPos,xtarget,ytarget,time)
+            obj.position.xPos = xPos0 + obj.speed*time*obj.targetUnitVector(1);
+            obj.position.yPos = yPos0 + obj.speed*time*obj.targetUnitVector(2);
             obj.position.dxPos = abs(xtarget - xPos);
             obj.position.dyPos = abs(ytarget - yPos);
         end
 
-        function obj = searchMotion(obj,xPos0,yPos0,xPos,yPos,xtarget,ytarget,time,targetUnitVector,assets)
-            turnRadius = 30;
-            angleVelo = obj.speed/turnRadius;                                    % hard coded turning radius (5)
-            angle = angleVelo*time;
-
+        function obj = searchMotion(obj,xPos0,yPos0,xPos,yPos,xtarget,ytarget,time,assets)
             for n = 1:length(assets)                                        % Determine if the asset is in range of the UAS
                 assetDistance(n) = norm([xPos, yPos] - assets(n).location);
 
@@ -33,31 +30,26 @@ classdef UAS < handle
             [distance, idx] = min(assetDistance);                           % Determine which asset is closer if more than one is in range
 
             if distance <= 20
-                turn = turnRadius*(1-cos(angle));
-                straight = turnRadius*sin(angle);
-                targetVector = targetUnitVector*obj.speed;
-                targetAngle = atan2d(targetVector(2),targetVector(1));
-                relitaveAsset = assets(idx).location - [xPos,yPos];
+                turnRadius = 5;
+                angleVelo = obj.speed/turnRadius;
+                angle = angleVelo*0.05;
 
-                for n = 1:2
-                    if relativeAsset(n) > 0
-                        vectorChange(n) = targetVector(n) + speedChange(n);
-                    else
-                        vectorChange(n) = targetVector(n) - speedChange(n);
-                    end
+                targetAngle = atan2(assets(idx).location(1) - obj.position.xPos,assets(idx).location(2) - obj.position.yPos);
+                
+                if targetAngle < 0
+                    DCM = [cos(angle) -sin(angle);sin(angle) cos(angle)];
+                elseif targetAngle > 0
+                    DCM = [cos(-angle) -sin(-angle);sin(-angle) cos(-angle)];
                 end
-                targetUnitVector = (targetVector + speedChange)/norm(targetVector + speedChange);
-                obj.position.xPos = xPos0 + obj.speed*time*targetUnitVector(1);
-                obj.position.yPos = yPos0 + obj.speed*time*targetUnitVector(2);
-                obj.position.dxPos = abs(xtarget - xPos);
-                obj.position.dyPos = abs(ytarget - yPos);
 
+                obj.targetUnitVector = DCM*obj.targetUnitVector;
+                obj.position.xPos = obj.position.xPos + obj.speed*0.05*obj.targetUnitVector(1);
+                obj.position.yPos = obj.position.yPos + obj.speed*0.05*obj.targetUnitVector(2);
 
             else
-                obj.position.xPos = xPos0 + obj.speed*time*targetUnitVector(1);
-                obj.position.yPos = yPos0 + obj.speed*time*targetUnitVector(2);
-                obj.position.dxPos = abs(xtarget - xPos);
-                obj.position.dyPos = abs(ytarget - yPos);
+                obj.position.xPos = xPos + obj.speed*0.05*obj.targetUnitVector(1);
+                obj.position.yPos = yPos + obj.speed*0.05*obj.targetUnitVector(2);
+                
             end
 
         end
